e6148c31bbcbe9cb103479fba90f86f073a6e24a
Allegra <aukseallegra@gmail.com>
2018-06-27 12:48:27 +0100

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,45 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  get '/' do
+
+  get '/recipes' do      #shows all recipes
+    @recipes = Recipe.all
     erb :index
   end
+
+  get '/recipes/new' do   #form to create a new one
+    erb :new
+  end
+
+  post '/recipes' do   #which gets created here and redirected to show the item page
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do      #shows a single recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do      #edit the recipe
+    @recipe = Recipe.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do     #updates from the form
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save
+    erb :show    #shows the updates post
+    #or use update?
+  end
+
+  delete '/recipes/:id/delete' do
+    @recipe = Recipe.find_by_id(params[:id])
+    @recipe.delete
+    erb :deleted
+  end
+
 end
